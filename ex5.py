ea8ccb7d1f62c73a5da9c16e12c8aa113b8a2cb7
Lemon <mail.wen.t@gmail.com>
2015-11-30 22:02:17 +0800

    learning code

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eye = 'blue'
+my_teeth = 'white'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eye, my_hair)
+print "His teeth are usually %s depending on the cooffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
